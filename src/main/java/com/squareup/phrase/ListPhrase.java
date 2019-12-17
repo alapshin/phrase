4b66d033bb7dedee3ad30848b703c2789c9fd72a
Andrei Lapshin <321946+alapshin@users.noreply.github.com>
2019-12-17 19:08:43 +0300

    Update dependencies (#1)
    * Update gradle and android gradle plugin
    
    * Bump compileSdkVersion and minSdkVersion, remove buildToolsVersion
    
    * Use implementation to declare dependencies instead of deprecated compile
    
    * Migrate to AndroidX
    
    * Update test dependencies
    
    * Add GitHub Actions CI pipeline

--- src/main/java/com/squareup/phrase/ListPhrase.java
+++ src/main/java/com/squareup/phrase/ListPhrase.java
@@ -15,8 +15,8 @@
  */
 package com.squareup.phrase;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import java.util.AbstractList;
 import java.util.Collection;
 import java.util.Iterator;
